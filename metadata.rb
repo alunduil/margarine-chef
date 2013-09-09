095ff37be8e423785c7f5632589af2abbc20f130
Hart Hoover <hart.hoover@gmail.com>
2013-09-09 11:17:31 -0500

    Add recipe descriptions to metadata.rb, #1

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,21 @@ version "1.0.0"
 description "This tool adds features to an amalgamation of delicious, the social bookmarking application, and readability, the excellent web article renderer."
 maintainer "Hart Hoover"
 maintainer_email "hart.hoover@rackspace.com"
+
 depends "python"
 depends "apt"
+
+supports "ubuntu"
+
+recipe "margarine::default",
+	"Installs and configures services that are common to each margarine service."
+
+recipe "margarine::blend",
+	"Installs the Blend service."
+
+recipe "margarine::spread",
+	"Installs the spread service."
+
+recipe "margarine::tinge",
+	"Installs the tinge service."
+
