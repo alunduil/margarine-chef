5841ae22a292e5910f63bb24182ab254dbce1e67
Alex Brandt <alunduil@alunduil.com>
2013-10-03 22:22:54 -0500

    Update version for flask inclusion.
    Issues: #10.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "hart.hoover@rackspace.com"
 license          "as-is"
 description      "Installs and configures margarine's components for deployment."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.0"
+version          "2.1.0"
 
 supports 'ubuntu'
 
